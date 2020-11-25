1965dd52d2d7876df9d7b2138bc93906919ac2e4
rk0576 <70932901+rk0576@users.noreply.github.com>
2020-11-25 11:40:17 +0300

    Add files via upload

---
+++ string3.py
@@ -0,0 +1,6 @@
+cuvantul1=str(input('Primul cuvant este :'))
+cuvantul2=str(input('Al doilea cuvant este :'))
+cuvantul3=str(input('Al treilea cuvant este :'))
+cuvantul4=str(input('Al patrulea cuvant este :'))
+fraza=cuvantul1 + ' ' + cuvantul2 + ' ' + cuvantul3 + ' ' + cuvantul4  + '.'
+print('Fraza este :' , fraza)
